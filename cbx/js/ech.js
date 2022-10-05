3f512b16b557eb1a232d2a62efef951d21a6a2f0
wyyis <wyyi2051@icloud.com>
2022-10-05 10:56:53 +0800

    cbx

---
+++ cbx/js/ech.js
@@ -0,0 +1,23 @@
+let nowClientWidth = document.documentElement.clientWidth;
+ 
+ 
+function nowSize(val,initWidth=1242){
+    return val * (nowClientWidth/initWidth)*77.625;
+};
+
+
+
+window.onload = function () {
+        
+        
+        
+}
+
+
+
+
+
+
+       
+
+ 
